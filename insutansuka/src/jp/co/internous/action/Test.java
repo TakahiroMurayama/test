44c1a9653d882fa3d57ccd0ba0f9cc9e047949d4
mayuowl <my-hrt@i.softbank.jp>
2018-12-07 21:44:08 +0900

    12/7

---
+++ insutansuka/src/jp/co/internous/action/Test.java
@@ -0,0 +1,87 @@
+package jp.co.internous.action;
+
+public class Test {
+	public static void main(String[]args){
+		Person taro= new Person();
+		taro.name= "山田太郎";
+		taro.age= 20;
+		taro.phoneNumber="080-3333-2211";
+		taro.address="渋谷";
+		taro.talk();
+		taro.walk();
+		taro.run();
+
+
+
+		System.out.println(taro.name);
+		System.out.println(taro.age);
+		System.out.println(taro.phoneNumber);
+		System.out.println(taro.address);
+
+		Person jiro= new Person();
+		jiro.name="木村次郎";
+		jiro.age=18;
+		jiro.phoneNumber= "090-2211-2211";
+		jiro.address= "目黒";
+		jiro.talk();
+		jiro.walk();
+		jiro.run();
+
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
+		System.out.println(jiro.phoneNumber);
+		System.out.println(jiro.address);
+
+		Person hanako= new Person();
+		hanako.name="鈴木花子";
+		hanako.age=16;
+		hanako.phoneNumber= "090-1111-2222";
+		hanako.address= "銀座";
+		hanako.talk();
+		hanako.walk();
+		hanako.run();
+
+		System.out.println(hanako.name);
+		System.out.println(hanako.age);
+		System.out.println(hanako.phoneNumber);
+		System.out.println(hanako.address);
+
+
+		Person takahiro= new Person();
+		takahiro.name="村山隆宏";
+		takahiro.age=23;
+		takahiro.phoneNumber="080-3333-4444";
+		takahiro.address="六本木";
+		takahiro.talk();
+		takahiro.walk();
+		takahiro.run();
+
+
+		System.out.println(takahiro.name);
+		System.out.println(takahiro.age);
+		System.out.println(takahiro.phoneNumber);
+		System.out.println(takahiro.address);
+
+		Robot aibo =new Robot();
+		aibo.name="aibo";
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
+
+		Robot asimo= new Robot();
+		asimo.name="asimo";
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
+
+		Robot pepper= new Robot();
+		pepper.name="pepper";
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
+
+
+
+	}
+
+}
